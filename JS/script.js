adf069a46e495702345519c72d732de02e5ef58a
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-11 19:51:47 +0200

    added method generateEmails

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,30 @@
 console.log('JS OK')
 
+const { createApp } = Vue
 createApp({
     data: () => ({
-
+        apiUrl: "https://flynn.boolean.careers/exercises/api/random/mail",
     }),
-    computed: {},
-    methods: {},
+    emails: '',
+    computed: {
+    },
+    methods: {
+
+
+        gererateEmails() {
+            for (let i = 0; i <= 10; i++) {
+                axios.get(this.apiUrl)
+                    .then((response) => {
+                        this.emails = response.data.response;
+                        console.log(this.emails)
+                    });
+            }
+
+        },
+
+
+    },
+    mounted() {
+        this.gererateEmails()
+    },
 }).mount('#app')
